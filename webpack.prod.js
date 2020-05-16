eaa70015e9a6ca7ec844c08f33c81ad9a4c1000f
moccands <moccandsayeghsara@gmail.com>
2020-05-16 08:30:04 +0200

    all css is minify

--- webpack.prod.js
+++ webpack.prod.js
@@ -27,6 +27,8 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const OptimizeCssAssetsWebpackPlugin = require ("optimize-css-assets-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -34,10 +36,12 @@ module.exports = {
         libraryTarget: 'var',
         library: 'Client'
     },
-    /*mode: 'development',*/
+
     mode: 'production',
-   /* devtool: 'source-map',
-    stats: 'verbose',*/
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({}), new OptimizeCssAssetsWebpackPlugin({})],
+      },
     module: {
 
         rules: [
